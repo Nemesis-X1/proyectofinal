7ce5cc9c87278a8ef4ac9656aa133ff7137ad92c
marcelo <tronkito0230@gmail.com>
2026-03-05 00:18:26 -0400

    Pocos Cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,11 +65,9 @@
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2022</div>
+                        <div class="text-muted">Copyright &copy; Website 2026</div>
                         <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
+                            <a href="https://wa.me/qr/RIWPK4XXO57ZI1">Soporte</a>
                         </div>
                     </div>
                 </div>
